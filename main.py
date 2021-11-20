1d7d0099fcef22f3423fab974c4edb14d50030fb
Vannucci <homelete@gmail.com>
2021-11-20 00:14:02 -0300

    login e anuncio funcionando

--- main.py
+++ main.py
@@ -7,18 +7,19 @@ from selenium.webdriver.common.keys import Keys
 from  time import sleep as sleep
 
 from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.chrome.options import Options
 import password
 import sqlite3
 
 
 url = 'https://www2.olx.com.br/desapega'
 
-#options = Options()
+options = Options()
 #options.headless = True
-#options.add_argument("--window-size=1920,1200")
+options.add_argument("user-data-dir=C:\\webdriver\\selenium")
 
 PATH = 'C:\webdriver\chromedriver.exe'
-browser = webdriver.Chrome(executable_path = PATH) # (options=options, executable_path=DRIVER_PATH)
+browser = webdriver.Chrome (options=options, executable_path=PATH) # (executable_path = PATH)
 browser.get(url)
 
 palavra = password.username
@@ -53,6 +54,7 @@ def anuncio():
     '''
     Função insere o anúncio no formulário
     '''    
+    print('Anuncio started')
     cat = 'Música e hobbies'
     sub_cat = 'Livros e revistas'
     tit = 'teste'
@@ -62,54 +64,79 @@ def anuncio():
 
     categoria = ['Música e hobbies', 'Eletrônicos e celulares']
     sub_categoria = ['Livros e revistas', 'Cds e dvds']
-    url = 'https://www2.olx.com.br/ai/form/0/'
+    url = 'https://www2.olx.com.br/ai/form/0/' 
     titulo = browser.find_element_by_xpath('//*[@id="input_subject"]')
-    descricao = browser.find_element_by_xpath('//*[@id="input_body"]')
-    preco = browser.find_element_by_xpath('//*[@id="price"]')
-    enviar_anuncio = browser.find_element_by_xpath('//*[@id="ad_insertion_submit_button"]')
+    descricao = browser.find_element_by_xpath('//*[@id="input_body"]')    
+    enviar_anuncio = browser.find_element_by_xpath('/html/body/div[1]/div[2]/form/div[2]/div/button') #//*[@id="ad_insertion_submit_button"]
+
     if browser.current_url == url:
+        titulo.send_keys(tit)
+        sleep(3)
+        print('titulo')
+
+        descricao.send_keys(anuncio)
+        sleep(3)
+        print('descricao')
         
         if cat in categoria:
             cat_musica_hobbies = browser.find_element_by_xpath('//*[@id="category_item-11000"]')
+            cat_musica_hobbies.click()
+            sleep(3)
             
         if sub_cat in sub_categoria:
             sub_cat_livros = browser.find_element_by_xpath('//*[@id="category_item-11060"]')
-
-        titulo.send_keys(tit)
-        descricao.send_keys(anuncio)
+            sub_cat_livros.click()
+            sleep(3)
+        
+        
+        
+        preco = browser.find_element_by_xpath('//*[@id="price"]') 
+        sleep(3)        
         preco.send_keys(prec)
+        print('preco')
+        sleep(3)
+        browser.find_element_by_xpath('//*[@id="cookie-notice-ok-button"]').click() # Aceito cookies
         enviar_anuncio.click()
-
+        sleep(20)
+        print('sem destaque')
+        browser.find_element_by_xpath('//*[@id="root"]/section/div/a').click() # Nao aceito pagar pelo anuncio
+    print('Anuncio ended')
 
 
 def login():
     '''
     Função que faz o login no site
     '''
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.support import expected_conditions as EC
+    print('Login started')
+    sleep(5)
+    url = 'https://www2.olx.com.br/ai/form/0/'
 
-    email = browser.find_element_by_xpath('//*[@id="__next"]/div/div[1]/div[1]/div[2]/form/div[1]/div[2]/input')
-    password = browser.find_element_by_xpath('//*[@id="__next"]/div/div[1]/div[1]/div[2]/form/div[2]/div[2]/div/div/input')
-    submit = browser.find_element_by_xpath('//*[@id="__next"]/div/div[1]/div[1]/div[2]/form/button')   
-    
-    if email:
-         email.send_keys(palavra)
-         print('usuario')
-    if password:
-         password.send_keys(senha)
-         print('senha')
-         sleep(3)
-         submit.click()
-         print('click')
-    if browser.find_element_by_xpath('//*[@id="__next"]/div/div/div[1]/div/div[1]/span'):
-        browser.find_element_by_xpath('//*[@id="__next"]/div/div/div[1]/div/div[2]/form/button').click()
-        WebDriverWait(browser, 30).until(
+    if not url:   
+        from selenium.webdriver.common.by import By
+        from selenium.webdriver.support.ui import WebDriverWait
+        from selenium.webdriver.support import expected_conditions as EC
+
+        email = browser.find_element_by_xpath('//*[@id="__next"]/div/div[1]/div[1]/div[2]/form/div[1]/div[2]/input')
+        password = browser.find_element_by_xpath('//*[@id="__next"]/div/div[1]/div[1]/div[2]/form/div[2]/div[2]/div/div/input')
+        submit = browser.find_element_by_xpath('//*[@id="__next"]/div/div[1]/div[1]/div[2]/form/button')                 
+           
+        if email:
+            email.send_keys(palavra)
+            print('usuario')
+        if password:
+            password.send_keys(senha)
+            print('senha')
+            sleep(3)
+            submit.click()
+            print('click')
+                # https://selenium-python.readthedocs.io/waits.html
+        if browser.find_element_by_xpath('//*[@id="__next"]/div/div/div[1]/div/div[1]/span'):
+            browser.find_element_by_xpath('//*[@id="__next"]/div/div/div[1]/div/div[2]/form/button').click()
+            WebDriverWait(browser, 30).until(
             EC.presence_of_element_located((By.xpath,'//*[@id="__next"]/div/div/div[1]/div/div[2]/form/div[2]/button'))
             )
-        browser.find_element_by_xpath('//*[@id="__next"]/div/div/div[1]/div/div[1]/span').click()
-
+            browser.find_element_by_xpath('//*[@id="__next"]/div/div/div[1]/div/div[1]/span').click()
+    print('Login ended')
 
 
     
@@ -146,13 +173,7 @@ Passos registra anúncio básico
 6 - submeter
 '''
 login()
-sleep(20)
 anuncio()
-sleep(20)
-sleep(20)
-sleep(20)
-
-
 
 print('saindo')
 browser.quit()
