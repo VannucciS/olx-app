54627724ab2234528927802f9a34cb805f6184b5
Vannucci <homelete@gmail.com>
2021-11-30 22:59:07 -0300

    função limpar e conexão ao bd realizadas

--- new_interface.py
+++ new_interface.py
@@ -1,15 +1,62 @@
 from tkinter import *
+from tkinter import ttk
+import sqlite3
 
 root = Tk()
 
+class Functions():
+    def conecta_bd(self):
+        self.conn = sqlite3.connect("anuncios.bd")
+        self.cursor = self.conn.cursor()   
+        print('Conectando ao banco de dados')
 
-class app():
+    def desconecta_bd(self):
+        self.conn.close(); print('Conexão com o BD perdida...')
+
+    def monta_tabelas(self):
+        self.conecta_bd()
+        self.cursor.execute("""
+    CREATE TABLE IF NOT EXISTS anuncios
+    (
+        cod INTEGER PRIMARY KEY, 
+        titulo CHAR(40) NOT NULL, 
+        texto CHAR(200), 
+        preco INTEGER(8),
+        categoria CHAR(30),
+        sub_categoria CHAR(30),
+        foto1 CHAR(200),
+        foto2 CHAR(200),
+        foto3 CHAR(200),
+        foto4 CHAR(200),
+        foto5 CHAR(200)
+        );
+    """)
+        self.conn.commit(); print("BD criado")
+        self.desconecta_bd()
+
+    def limpa_tela(self):
+        self.codigo_entry.delete(0, END)
+        self.tit_entry.delete(0, END)
+        self.preco_entry.delete(0, END)
+        self.texto_entry.delete('1.0', 'end-1c')        
+    def buscar(self):
+        pass
+    def novo(self):
+        pass
+    def alterar(self):
+        pass
+    def apagar(self):
+        pass
+
+class App(Functions):
     def __init__(self) :
         self.root = root
         self.tela()
         self.frames_da_tela()
         self.criando_botoes()
         self.label_entries()
+        self.tree_view()
+        self.monta_tabelas()
         root.mainloop()
     
     def tela(self):
@@ -28,46 +75,46 @@ class app():
 
     def criando_botoes(self):
         ## criação dos botao limpar
-        self.bt_limpar = Button(self.frame_1, text = 'Limpar')
+        self.bt_limpar = Button(self.frame_1, text = 'Limpar', bd = 3, bg="#107db2", fg= 'white',font=('arial', 10, 'bold'), command=self.limpa_tela)
         self.bt_limpar.place(relx=0.2, rely=0.1, relwidth=0.1, relheight=0.15)
         ## criação dos botao buscar
-        self.bt_busca = Button(self.frame_1, text = 'Buscar')
+        self.bt_busca = Button(self.frame_1, text = 'Buscar',  bd = 3, bg="#107db2", fg= 'white', font=('arial', 10, 'bold'))
         self.bt_busca.place(relx=0.3, rely=0.1, relwidth=0.1, relheight=0.15)
          ## criação dos botao novo
-        self.bt_novo = Button(self.frame_1, text = 'novo')
+        self.bt_novo = Button(self.frame_1, text = 'Novo',  bd = 3, bg="#107db2", fg= 'white', font=('arial', 10, 'bold'))
         self.bt_novo.place(relx=0.5, rely=0.1, relwidth=0.1, relheight=0.15)
          ## criação dos botao alterar
-        self.bt_alterar = Button(self.frame_1, text = 'alterar')
+        self.bt_alterar = Button(self.frame_1, text = 'Alterar',  bd = 3, bg="#107db2", fg= 'white', font=('arial', 10, 'bold'))
         self.bt_alterar.place(relx=0.6, rely=0.1, relwidth=0.1, relheight=0.15)
          ## criação dos botao apagar
-        self.bt_apagar = Button(self.frame_1, text = 'apagar')
+        self.bt_apagar = Button(self.frame_1, text = 'Apagar',  bd = 3, bg="#107db2", fg= 'white', font=('arial', 10, 'bold'))
         self.bt_apagar.place(relx=0.7, rely=0.1, relwidth=0.1, relheight=0.15)
 
     def label_entries(self):
         ## criação label e entrada código
-        self.lb_codigo = Label(self.frame_1, text='Código')
-        self.lb_codigo.place(relx=0.05, rely=0.05, relwidth=0.08)#, relwidth=0.1, relheight=0.15)
+        self.codigo = Label(self.frame_1, text='Código', bg='lightgray')
+        self.codigo.place(relx=0.05, rely=0.05, relwidth=0.08)#, relwidth=0.1, relheight=0.15)
 
         self.codigo_entry = Entry(self.frame_1)
         self.codigo_entry.place(relx=0.05, rely=0.16, relwidth=0.08)
 
         ## criação label título
-        self.lb_tit = Label(self.frame_1, text='Título')
-        self.lb_tit.place(relx=0.05, rely=0.3, relwidth=0.08)
+        self.tit = Label(self.frame_1, text='Título', bg='lightgray')
+        self.tit.place(relx=0.05, rely=0.3, relwidth=0.08)
         
         self.tit_entry = Entry(self.frame_1)
         self.tit_entry.place(relx=0.15, rely=0.3, relwidth=0.48)
 
         ## criação label preco
-        self.lb_preco = Label(self.frame_1, text='Preço')
-        self.lb_preco.place(relx=0.65, rely=0.3, relwidth=0.06)
+        self.preco = Label(self.frame_1, text='Preço', bg='lightgray')
+        self.preco.place(relx=0.65, rely=0.3, relwidth=0.06)
 
-        self.codigo_entry = Entry(self.frame_1)
-        self.codigo_entry.place(relx=0.73, rely=0.3, relwidth=0.1)
+        self.preco_entry = Entry(self.frame_1)
+        self.preco_entry.place(relx=0.73, rely=0.3, relwidth=0.1)
 
         ## criação label texto
-        self.lb_texto = Label(self.frame_1, text='Texto')
-        self.lb_texto.place(relx=0.05, rely=0.5, relwidth=0.06)
+        self.texto = Label(self.frame_1, text='Texto', bg='lightgray')
+        self.texto.place(relx=0.05, rely=0.5, relwidth=0.06)
 
         self.texto_entry = Text(self.frame_1,height=2, width=50) 
         self.texto_entry.place(relx=0.15, rely=0.5, relwidth=0.7)
@@ -75,7 +122,30 @@ class app():
     def combobox(self):
         pass
      
+    def tree_view(self):
+        self.listacli = ttk.Treeview(self.frame_2, height=3, columns=('col1', 'col2','col3', 'col4'), show='headings')
 
+        # Nome de cada coluna
+        self.listacli.heading("#0", text="")
+        self.listacli.heading("#1", text="Código")
+        self.listacli.heading("#2", text="Titulo")
+        self.listacli.heading("#3", text="Preço")
+        self.listacli.heading("#4", text="Texto")
 
-app()
+        # Tamanho de cada coluna
+        self.listacli.column('#0', width=1) # Faz com que a primeira coluna não apareça no treeview , stretch=NO
+        self.listacli.column('#1', width=50)
+        self.listacli.column('#2', width=200)
+        self.listacli.column('#3', width=125)
+        self.listacli.column('#4', width=125)
+
+        # Posição do widget no frame
+        self.listacli.place(relx=0.01, rely=0.01, relheight=0.85, relwidth=0.97)
+
+        # Barra de rolagem lateral
+        self.scrollbar = Scrollbar(self.frame_2, orient='vertical', command=self.listacli.yview)
+        self.listacli.configure(yscroll=self.scrollbar.set)
+        self.scrollbar.place(relx=0.98, rely=0.01, relwidth=0.04, relheight=0.85)
+
+App()
 
